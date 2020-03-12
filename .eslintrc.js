e4a5edc6ce91d593d2d1c300dcd2f3056196f53b
SevenCatsHere <snowcolton@hotmail.com>
2020-03-12 02:10:39 -0400

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  root: true,
+	extends: 'airbnb-base',
+	env: { node: true },
+	rules: {
+		strict: [2, 'error'],
+		indent: [2, 'tab'],
+		'no-tabs': 0,
+	},
+  overrides: [
+    {},
+  ],
+};
