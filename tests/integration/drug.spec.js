9b9a69701ff1447696784873221accc9579510e6
SevenCatsHere <snowcolton@hotmail.com>
2020-03-15 01:32:19 -0400

    check DB contents

--- tests/integration/drug.spec.js
+++ tests/integration/drug.spec.js
@@ -2,7 +2,14 @@
 
 const { gql } = require('apollo-server');
 const { createTestClient } = require('apollo-server-testing');
+const knex = require('knex');
+const knexConfig = require('../../knexfile');
 const { createTestServer, uuidRegex, isoDateRegex } = require('./utils');
+
+let db;
+beforeAll(() => {
+	db = knex(knexConfig);
+});
 
 describe('Mutations', () => {
 	test('Can create a new drug', async () => {
@@ -52,6 +59,20 @@ describe('Mutations', () => {
 			detection: null,
 			pubchemCid: null,
 			referencesAndNotes: null,
+		});
+
+		const record = await db('drug').where('id', id).first();
+		expect(record).toEqual({
+			id,
+			name: 'Lubetazerpine',
+			summary: 'Slippery when wet',
+			effects: 'Whoaaa',
+			avoid: null,
+			detection: null,
+			pubchemCid: null,
+			referencesAndNotes: null,
+			createdAt: new Date(createdAt),
+			updatedAt: new Date(updatedAt),
 		});
 	});
 });
